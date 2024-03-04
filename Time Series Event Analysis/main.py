3b254460c3ae6b4645d2843755da8d9d9bfa5663
Galactic Commander <95187806+sakib4535@users.noreply.github.com>
2024-03-04 08:16:02 +0600

    Add files via upload

---
+++ Time Series Event Analysis/main.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+import pandas as pd
+
+def fetch_and_save_data(ticker_symbol, start_date, end_date, output_file):
+    stock_data = yf.download(ticker_symbol, start=start_date, end=end_date)
+
+    if 'Date' in stock_data.index.names:
+        print("THe Date Column is an index")
+    else:
+        print("The Date column is not an index")
+
+    stock_data.to_csv(output_file)
+
+if __name__ == "__main__":
+    # Set the parameters
+    ticker_symbol = "AAPL"
+    start_date = "2022-01-01"
+    end_date = "2023-01-01"
+    output_file = "stock_data.csv"
+
+    # Fetch and save stock data
+    fetch_and_save_data(ticker_symbol, start_date, end_date, output_file)
